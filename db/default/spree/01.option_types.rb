55a012f1d7a138ed5b1616fc4322faaa0453e946
Pedro Quintero Rojas <pqr@uh.cu>
2014-12-09 07:48:35 -0500

    adding short field to seed data

--- db/default/spree/01.option_types.rb
+++ db/default/spree/01.option_types.rb
@@ -5,18 +5,18 @@
 # Con el tema de los modulos de viaje, la idea es que se compran dos viajes, y son dos
 # productos distintos, creo que con package se hace esto bastante bien
 option_types = [
-  {:name => "start_date", :presentation => "Start Date", :attr_type => 'date'},
-  {:name => "end_date", :presentation => "End Date", :attr_type => 'date'},
-  {:name => "adult", :presentation => "Adult", :attr_type => 'integer'},
-  {:name => "child", :presentation => "Child", :attr_type => 'integer'},
-  {:name => "one_child", :presentation => "One Child", :attr_type => 'float'},
-  {:name => "one_adult", :presentation => "One Adult", :attr_type => 'float'},
-  {:name => "departure_date", :presentation => "Departure Date", :attr_type => 'date'},
-  {:name => "return_date", :presentation => "Departure Date", :attr_type => 'date'},
-  {:name => "travel_class", :presentation => "Class", :attr_type => 'selection'},
-  {:name => "travel_module", :presentation => "Travel Module", :attr_type => 'selection'},
-  {:name => "origin", :presentation => "Origin", :attr_type => 'destination'},
-  {:name => "destination", :presentation => "Destination", :attr_type => 'destination'},
+  {:name => "start_date", :presentation => "Start Date", :attr_type => 'date', :short => 'Start'},
+  {:name => "end_date", :presentation => "End Date", :attr_type => 'date', :short => 'End'},
+  {:name => "adult", :presentation => "Adult", :attr_type => 'integer', :short => 'Adult'},
+  {:name => "child", :presentation => "Child", :attr_type => 'integer', :short => 'Child'},
+  {:name => "one_child", :presentation => "One Child", :attr_type => 'float', :short => 'Child'},
+  {:name => "one_adult", :presentation => "One Adult", :attr_type => 'float', :short => 'Adult'},
+  {:name => "departure_date", :presentation => "Departure Date", :attr_type => 'date', :short => 'Depart'},
+  {:name => "return_date", :presentation => "Return Date", :attr_type => 'date', :short => 'Return'},
+  {:name => "travel_class", :presentation => "Class", :attr_type => 'selection', :short => 'Class'},
+  {:name => "travel_module", :presentation => "Travel Module", :attr_type => 'selection', :short => 'Mod'},
+  {:name => "origin", :presentation => "Origin", :attr_type => 'destination', :short => 'Orig'},
+  {:name => "destination", :presentation => "Destination", :attr_type => 'destination', :short => 'Dest'},
 ]
 
 ### Creating Option Types
