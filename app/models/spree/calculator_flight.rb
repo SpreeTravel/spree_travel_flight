e652327567ff1412f70b67341f62e0bc6bcee93f
Pedro Quintero <pecuerre@gmail.com>
2015-11-02 09:03:52 -0500

    working on the flight calculator

--- app/models/spree/calculator_flight.rb
+++ app/models/spree/calculator_flight.rb
@@ -4,19 +4,21 @@ module Spree
     def self.calculate_price(context, variant, options)
       return [] if variant.rates.empty?
       prices = []
-      days = context.end_date(options).to_date - context.start_date(options).to_date rescue 1
+      #days = context.end_date(options).to_date - context.start_date(options).to_date rescue 1
 
       variants.rates.each do |r|
-        next if context.departure_date.present? && (context.departure_date.to_date < r.start_date.to_date rescue false)
-        next if context.departure_date.present? && (context.departure_date.to_date > r.end_date.to_date rescue false)
-        next if context.origin.present? && (context.origin.presentation == r.origin.presentation rescue false)
-        next if context.destination.present? && (context.destination.presentation == r.destination.presentation rescue false)
-        adults_array = self.get_adult_list(r, context.adult)
-        children_array = self.get_child_list(r, context.child)
-        combinations = adults_array.product(children_array)
-        combinations.each do |ad, ch|
-          prices << self.get_rate_price(r, ad, ch)
-        end
+        #next if context.departure_date.present? && (context.departure_date.to_date < r.start_date.to_date rescue false)
+        #next if context.departure_date.present? && (context.departure_date.to_date > r.end_date.to_date rescue false)
+        #next if context.origin.present? && (context.origin.presentation == r.origin.presentation rescue false)
+        #next if context.destination.present? && (context.destination.presentation == r.destination.presentation rescue false)
+        #adults_array = self.get_adult_list(r, context.adult)
+        #children_array = self.get_child_list(r, context.child)
+        #combinations = adults_array.product(children_array)
+        #combinations.each do |ad, ch|
+        #  prices << self.get_rate_price(r, ad, ch)
+        #end
+        next if (context.departure_date.to_date != r.start_date.to_date rescue false)
+        prices << r
       end
       prices
     end
